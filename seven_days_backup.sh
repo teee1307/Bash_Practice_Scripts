d77c55c7f0e02cbba995b8724149f287765894e9
teee1307 <tanaya.s@crestinfosystems.com>
2024-01-11 17:23:48 +0530

    more scripts

---
+++ seven_days_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Specify the directory where you want to search for files
+path=$1
+
+# Use find to get the list of files and sort them based on the date in the filename
+files_to_keep=$(find "$search_directory" -type f -name "batch_log-*" | \
+  sort -t '-' -k 2 -r | \
+  head -n 7)
+
+# Use find to get the list of files to delete (exclude the ones to keep) grep -vE excludes
+files_to_delete=$(find "$search_directory" -type f -name "batch_log-*" | \
+  grep -vE "$(echo "$files_to_keep" | sed 's/$/|/' | tr -d '\n' | sed 's/|$//')")
+
+# Delete the files
+echo "$files_to_delete" | xargs rm
